5edf3e33be26a50765d4a16314797b674f08cbc4
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-03-19 18:47:35 -0500

    database connection configured

--- database.js
+++ database.js
@@ -1,34 +1,35 @@
-const { Pool } = require("pg");
-
-// Configure the connection parameters for your local PostgreSQL database
-const pool = new Pool({
-  user: "postgres", // Replace 'your_username' with your PostgreSQL username
-  host: "localhost", // Assuming your PostgreSQL server is running locally
-  database: "test_db", // Replace 'your_database' with your PostgreSQL database name
-  password: "celestial7000", // Replace 'your_password' with your PostgreSQL password
-  port: 5432, // Default PostgreSQL port
-});
-
-// Test the connection
-pool.connect((err) => {
-  if (err) {
-    console.error("Error connecting to PostgreSQL:", err);
-  } else {
-    console.log("Connected to PostgreSQL successfully!");
-  }
-});
-
-module.exports = pool;
-
-// const { Pool } = require('pg')
+const { Pool } = require('pg')
  
-// const pool = new Pool({
-//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-// })
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
-// pool.connect((err) => {
-//     if (err) throw err
-//     console.log("Connect to PostgreSQL successfully!")
-// })
+pool.connect((err) => {
+    if (err) throw err
+    console.log("Connect to PostgreSQL successfully!")
+})
 
 // module.exports = pool
+
+// //
+// const { Pool } = require("pg");
+
+// // Configure the connection parameters for your local PostgreSQL database
+// const pool = new Pool({
+//   user: "postgres", // Replace 'your_username' with your PostgreSQL username
+//   host: "localhost", // Assuming your PostgreSQL server is running locally
+//   database: "test_db", // Replace 'your_database' with your PostgreSQL database name
+//   password: "celestial7000", // Replace 'your_password' with your PostgreSQL password
+//   port: 5432, // Default PostgreSQL port
+// });
+
+// // Test the connection
+// pool.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to PostgreSQL:", err);
+//   } else {
+//     console.log("Connected to PostgreSQL successfully!");
+//   }
+// });
+
+// module.exports = pool;
